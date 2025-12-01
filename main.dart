af1dbc623790bb2f1958b60e8c58574da959dc14
rzzh10 <muhrizkyn10@gmail.com>
2025-12-01 21:28:43 +0700

    login admin dan dashboard

--- main.dart
+++ main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'login_admin_page.dart';
 import 'dashboard_admin_page.dart';
-
+import 'data_pendonor_page.dart';
+import 'stok_darah_page.dart';
+import 'jadwal_donor_page.dart';
 
 void main() {
   runApp(const DonorDarahApp());
@@ -20,6 +22,11 @@ class DonorDarahApp extends StatelessWidget {
       routes: {
         '/': (context) => const LoginAdminPage(),
         '/dashboard': (context) => const DashboardAdminPage(),
+
+        // Tambahan halaman baru
+        '/data-pendonor': (context) => const DataPendonorPage(),
+        '/stok-darah': (context) => const StokDarahPage(),
+        '/jadwal-donor': (context) => const JadwalDonorPage(),
       },
     );
   }
